a31d0d0b0b895c56f3a8e036eb6e9e3304529ad0
Jair Yara <jyara@oneago.com>
2021-07-31 23:06:09 -0500

    Encapsulate functions

--- figures/js/figures.js
+++ figures/js/figures.js
@@ -1,42 +1,41 @@
 // Square
 console.group('square');
-const sideSquare = 5;
-console.log('The side of the square measures: ' + sideSquare + 'cm');
 
-const perimeterSquare = sideSquare * 4;
-console.log('The perimeter of the square is: ' + perimeterSquare + 'cm');
+function perimeterSquare(side) {
+    return side * 4;
+}
 
-const areaSquare = sideSquare * sideSquare;
-console.log('The area of the square is: ' + areaSquare + 'cm^2');
+function areaSquare(side) {
+    return side * side;
+}
 console.groupEnd();
 
 // Triangle
 console.group('Triangle');
-const sideOneTriangle = 6;
-const sideTwoTriangle = 6;
-const baseTriangle = 4;
-console.log('The side of triangle measures: ' + sideOneTriangle + 'cm, ' + sideTwoTriangle + 'cm, and it base ', + baseTriangle + 'cm');
+function perimeterTriangle(side1, side2, base) {
+    return side1 + side2 + base;
+}
 
-const heightTriangle = 5.5;
-console.log('The height of the triangle is: ' + heightTriangle + 'cm');
-
-const perimeterTriangle = sideOneTriangle +  sideTwoTriangle + baseTriangle;
-console.log('The perimeter of the triangle is: ' + perimeterTriangle + 'cm');
-
-const areaTriangle = (baseTriangle * heightTriangle) / 2;
-console.log('The area of the triangle is: ' + areaTriangle + 'cm^2');
+function areaTriangle(base, height) {
+    return (base * height) / 2
+}
 console.groupEnd();
 
 // Circle
 console.group('circle');
-const PI = Math.round(Math.PI * 1000) / 1000;
-const diameterCircle = 10;
-const radioCircle = diameterCircle / 2;
+const PI = Math.PI;
 
-const perimeterCircle = Math.round((diameterCircle * PI) * 100) / 100;
-console.log('The perimeter of the circle is: ' + perimeterCircle + 'cm');
+function diameterCircle(radio) {
+    return radio * 2;
+}
 
-const areaCircle = PI * (radioCircle *  radioCircle);
-console.log('The area of the circle is: ' + areaCircle + 'cm^2');
+function perimeterCircle(radio) {
+    const diameter = diameterCircle(radio);
+    return diameter * PI;
+}
+
+function areaCircle(radio) {
+    return PI * (radio * radio);
+}
 
 console.groupEnd();
